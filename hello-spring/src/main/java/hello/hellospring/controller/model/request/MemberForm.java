a8ea9f94722828c8d59757fffb6fd58ff19402bd
leewoooo <leecoding2285@gmail.com>
2022-01-28 00:49:48 +0900

    ✨Add: controller, model

---
+++ hello-spring/src/main/java/hello/hellospring/controller/model/request/MemberForm.java
@@ -0,0 +1,13 @@
+package hello.hellospring.controller.model.request;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
